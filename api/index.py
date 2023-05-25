319e58daec191da0491924624e2ec738fcbc0c35
Lyle Shaw <me@lyle.net.cn>
2023-05-25 16:50:42 +0800

    fix

--- api/index.py
+++ api/index.py
@@ -1,13 +1,18 @@
+import requests
+import validators
+from bs4 import BeautifulSoup
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
 
-from api.google_search import get_serps
 from api.logger import logger
 
 app = FastAPI(
-    title="Google Search Plugin",
-    description="Plugin for searching through internet to find answers to questions and retrieve relevant information."
-                "Use it whenever a user asks something that might be found in Google or users ask to visit a website.",
+    title="Search Plugin",
+    description="This tool allows users to provide a URL(or URLs) "
+                "and optionally requests for interacting with, extracting specific information or how to do "
+                "with the content from the URL. Requests may include rewrite, translate, and others. "
+                "If there any requests, when accessing the /api/visit-web endpoint, the parameter 'user_has_request' "
+                "should be set to 'true. And if there's no any requests, 'user_has_request' should be set to 'false'.",
     version="1.0.0",
 )
 app.add_middleware(
@@ -19,32 +24,40 @@ app.add_middleware(
 )
 
 
+def get_text(text_path):
+    text = ""
+    url = text_path
+    if validators.url(url):
+        headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+                                 "Chrome/91.0.4472.124 Safari/537.36",}
+        response = requests.get(url, headers=headers)
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.content, "html.parser")
+            text = soup.get_text()
+        else:
+            raise ValueError(f"Invalid URL! Status code {response.status_code}.")
+    text = " ".join(text.split())
+    return text
+
+
 # Path: /api/search
 # Param: query: str
 # Return: resp: str
-@app.get("/api/query", description="Search for query, return search results in string.")
-async def search(query: str) -> str:
+@app.get("/api/search", description="if there is any keyword that can be searched in google, give them to me in "
+                                    "user's word only without any additional words, then link should be like "
+                                    "'https://www.google.com/search?q={keyword}'. If there is a URL, the link should "
+                                    "be this URL")
+async def search(link: str) -> str:
     logger.info("search start")
-    logger.info("HTTP /search request: %s", query)
-    max_lengh = 1000
-    try:
-        data = get_serps(query, domain="google.com", host_language="zh-CN")
-        resp = "search results as follow:\n"
-        if data:
-            for i in data:
-                resp += "Title: " + i['title'] + "Text: " + i["text"] + "\n"
-                if len(resp) > max_lengh:
-                    break
-        print(resp)
-    except Exception as e:
-        logger.error("search error: %s", e)
-        return "Something went wrong."
-    logger.info("HTTP /search data: %s", resp)
-    return resp
+    logger.info("HTTP /search request: %s", link)
+    text = get_text(link)
+
+    logger.info("HTTP /search data: %s", text)
+    return text
 
 
 # start server
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8001)
